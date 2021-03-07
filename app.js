04e5f6ffb7090afd31ab87442aab5d57f9f80a96
Yongyeon <70844774+lotus-yeon@users.noreply.github.com>
2021-03-07 21:07:30 +0900

    이미지 저장 기능

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const colors = document.getElementsByClassName("jsColor");
 const ctx = canvas.getContext("2d");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 
 canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
 canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = INITIAL_COLOR;
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -67,12 +70,25 @@ function handleCanvasClick() {
     }
 }
 
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick(event) {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS.png";
+    link.click();
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);  //오른쪽 마우스
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -83,4 +99,8 @@ if(range) { //range가 비어있을 수 있으니까 확인 차 작성
 
 if(mode) {
     mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick)
 }
